46ee4a171fe43ca191f89b126f1621c7ac5d4ee8
Sophia <ms.shb2004@mail.ru>
2025-12-31 01:00:27 +0300

    feat: Make the basic repository fully universal

--- src/ct_backend/database/repositories/base.py
+++ src/ct_backend/database/repositories/base.py
@@ -1,7 +1,10 @@
 import logging
-from typing import Type, Any, Optional, List, Dict
+from typing import Type, Any
 
-from sqlalchemy import select, update, delete, and_
+from sqlalchemy import select, update, delete, and_, insert
+from collections.abc import Sequence
+from sqlalchemy.sql import ColumnElement
+from sqlalchemy.orm import DeclarativeBase
 
 from ...database import async_session
 
@@ -9,123 +12,174 @@ logger = logging.getLogger(__name__)
 
 
 class BaseRepository:
-    def __init__(self, model: Type):
+    def __init__(self, model: Type[DeclarativeBase]):
         self.model = model
-
-    @staticmethod
-    async def _get_session():
-        """Генератор сессии для использования в контекстах."""
-
-        async with async_session() as session:
-            async with session.begin():
-                yield session
 
     # =========== GET методы ===========
 
-    async def get_by_id(self, id_: int) -> Optional[Any]:
-        """Получить по ID"""
-        async for session in self._get_session():
+    async def _get_by_id(self, id_: int, columns: Sequence | None = None) -> dict | None:
+        """
+        Получить по ID
+        :param id_: ID получаемой записи
+        :param columns: Поля, которые нужно получить. None - получаем все поля
+        :return: Словарь в формате: {Название_поля: Значение, ...}
+        """
+
+        async with async_session() as session:
             result = await session.execute(
-                select(self.model).where(self.model.id == id_)  # type: ignore
+                select(
+                    *(columns or self.model.__table__.columns)
+                ).where(self.model.id == id_)  # type: ignore[attr-defined]
             )
-            return result.scalar_one_or_none()
+            row = result.mappings().one_or_none()
+            return dict(row) if row else None
 
-    async def _get_by_field(self, field_name: str, value: Any) -> Optional[Any]:
-        """Получить по любому полю"""
-        async for session in self._get_session():
-            field = getattr(self.model, field_name, None)
-            if not field:
-                raise AttributeError(f"Field {field_name} not found in {self.model}")
+    async def _get_one(self, *, filters: Sequence[ColumnElement[bool]] | None = None, columns: Sequence | None = None) -> dict | None:
+        """
+        Получить одну строку
+        :param filters: По каким полям фильтровать
+        :param columns: Поля, которые нужно получить. None - получаем все поля
+        :return: Словарь в формате: {Название_поля: Значение, ...}
+        """
 
-            result = await session.execute(
-                select(self.model).where(field == value)
-            )
-            return result.scalar_one_or_none()
-
-    async def _get_all(self, filters: Optional[List] = None) -> List[Any] | None:
-        """Получить все записи с возможностью фильтрации"""
-        async for session in self._get_session():
-            query = select(self.model)
+        async with async_session() as session:
+            stmt = select(*(columns or self.model.__table__.columns))
 
             if filters:
-                query = query.where(and_(*filters))
+                stmt = stmt.where(and_(*filters))
 
-            result = await session.execute(query)
-            return result.scalars().all()
+            result = await session.execute(stmt)
+            row = result.mappings().one_or_none()
+            return dict(row) if row else None
+
+    async def _get_many(self, *, filters: Sequence[ColumnElement[bool]] | None = None, columns: Sequence | None = None) -> list[dict]:
+        """
+        Получить множество строку
+        :param filters: По каким полям фильтровать
+        :param columns: Поля, которые нужно получить. None - получаем все поля
+        :return: Список словарей в формате: [{Название_поля: Значение, ...}, ...]
+        """
+
+        async with async_session() as session:
+            stmt = select(*(columns or self.model.__table__.columns))
+
+            if filters:
+                stmt = stmt.where(and_(*filters))
+
+            result = await session.execute(stmt)
+            return [dict(row) for row in result.mappings().all()]
 
     # =========== CREATE методы ===========
 
-    async def create(self, data: Dict) -> Any | None:
-        """Создать новую запись"""
+    async def create(self, data: dict[str, Any]) -> int:
+        """
+        Создать новую запись
+        :param data: Данные для заполнения в формате словаря
+        :return: ID новой записи
+        :exception Exception: Ошибка при занесении данных в базу (конфликт)
+        """
+
         try:
-            async for session in self._get_session():
-                instance = self.model(**data)
-                session.add(instance)
-                return instance
-        except Exception as error:
-            logger.error(f"Create error: {error}")
+            async with async_session() as session:
+                async with session.begin():
+                    stmt = (
+                        insert(self.model)
+                        .values(**data)
+                        .returning(self.model.id)  # type: ignore[attr-defined]
+                    )
+                    result = await session.execute(stmt)
+                    return result.scalar_one()
+
+        except Exception:
+            logger.exception(f'Create error')
             raise
 
-    async def bulk_create(self, items: List[Dict]) -> List[Any] | None:
-        """Создать несколько записей за раз"""
+
+    async def bulk_create(self, items: list[dict[str, Any]]) -> list[int]:
+        """
+        Создать несколько новых записей за одно обращение к БД
+        :param items: Данные для заполнения в формате списка словарей
+        :return: Список ID новых записей
+        :exception Exception: Ошибка при занесении данных в базу (конфликт)
+        """
+
         try:
-            async for session in self._get_session():
-                instances = [self.model(**item) for item in items]
-                session.add_all(instances)
-                return instances
-        except Exception as error:
-            logger.error(f"Bulk create error: {error}")
+            async with async_session() as session:
+                async with session.begin():
+                    stmt = (
+                        insert(self.model)
+                        .values(items)
+                        .returning(self.model.id)  # type: ignore[attr-defined]
+                    )
+                    result = await session.execute(stmt)
+                    return result.scalars().all()
+
+        except Exception:
+            logger.exception(f'Bulk create error')
             raise
 
     # =========== UPDATE методы ===========
 
-    async def update_by_id(self, id_: int, data: Dict) -> bool | None:
-        """Обновить запись по ID"""
-        async for session in self._get_session():
-            result = await session.execute(
-                select(self.model).where(self.model.id == id_).with_for_update()  # type: ignore
-            )
-            instance = result.scalar_one_or_none()
+    async def update_by_id(self, id_: int, values: dict[str, Any]) -> bool:
+        """
+        Обновить запись по ID
+        :param id_: ID обновляемой записи
+        :param values: Обновляемые данные в формате словаря
+        :return: True - если была обновлена одна строка, False - остальное
+        """
 
-            if instance:
-                for key, value in data.items():
-                    setattr(instance, key, value)
-                return True
-            return False
+        async with async_session() as session:
+            async with session.begin():
+                stmt = (
+                    update(self.model)
+                    .where(self.model.id == id_)  # type: ignore[attr-defined]
+                    .values(**values)
+                )
+                result = await session.execute(stmt)
+                return result.rowcount == 1
 
-    async def _update_where(self, values: Dict, filters: List | None = None) -> int | None:
-        """Обновить записи с возможностью фильтрации"""
-        async for session in self._get_session():
-            query = update(self.model).values(**values)
+    async def _update_where(self, *, values: dict[str, Any], filters: Sequence[ColumnElement[bool]]) -> int:
+        """
+        Обновить записи с фильтрацией
+        :param values: Обновляемые данные в формате словаря
+        :param filters: По каким полям фильтровать обновление
+        :return: Количество затронутых операцией строк (даже если значение не изменилось)
+        """
 
-            if filters:
-                query = query.where(and_(*filters))
-
-            result = await session.execute(query)
-            return result.rowcount
+        async with async_session() as session:
+            async with session.begin():
+                stmt = (
+                    update(self.model)
+                    .values(**values)
+                    .where(and_(*filters))
+                )
+                result = await session.execute(stmt)
+                return result.rowcount
 
     # =========== DELETE методы ===========
 
-    async def delete_by_id(self, id_: int) -> bool | None:
-        """Удалить запись по ID"""
-        async for session in self._get_session():
-            result = await session.execute(
-                select(self.model).where(self.model.id == id_)  # type: ignore
-            )
-            instance = result.scalar_one_or_none()
+    async def delete_by_id(self, id_: int) -> bool:
+        """
+        Удалить запись по ID
+        :param id_: ID удаляемой записи
+        :return: True - если была удалена одна строка, False - остальное
+        """
 
-            if instance:
-                await session.delete(instance)
-                return True
-            return False
+        async with async_session() as session:
+            async with session.begin():
+                stmt = delete(self.model).where(self.model.id == id_)  # type: ignore[attr-defined]
+                result = await session.execute(stmt)
+                return result.rowcount == 1
 
-    async def _delete_where(self, filters: List | None = None) -> int | None:
-        """Удалить записи с возможностью фильтрации"""
-        async for session in self._get_session():
-            query = delete(self.model)
+    async def _delete_where(self, filters: Sequence[ColumnElement[bool]]) -> int:
+        """
+        Удалить записи с фильтрацией
+        :param filters: По каким полям фильтровать удаление
+        :return: Количество затронутых операцией строк
+        """
 
-            if filters:
-                query = query.where(and_(*filters))
-
-            result = await session.execute(query)
-            return result.rowcount
+        async with async_session() as session:
+            async with session.begin():
+                stmt = delete(self.model).where(and_(*filters))
+                result = await session.execute(stmt)
+                return result.rowcount
--- src/ct_backend/database/repositories/user_repo.py
+++ src/ct_backend/database/repositories/user_repo.py
@@ -1,13 +1,21 @@
 from .base import BaseRepository
 from ..models.user import User
 
+from sqlalchemy import or_
+
 
 class UserRepository(BaseRepository):
     def __init__(self):
         super().__init__(User)
 
-    async def get_by_email(self, email: str):
-        return await self._get_by_field("email", email)
+    async def get_by_email(self, user_title: str):
+        return await self._get_one(
+            filters=[or_(
+                User.email == user_title,
+                User.username == user_title
+            )],
+            columns=[User.id]
+        )
 
 
 user_repo = UserRepository()
