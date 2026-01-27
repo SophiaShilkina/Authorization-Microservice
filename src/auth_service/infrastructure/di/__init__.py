74a1feefd7a061037b1c5c2e278aac8dffefc961
Sophia <ms.shb2004@mail.ru>
2026-01-27 10:30:41 +0300

    refactor: Make container creation a function, pass in the config

---
+++ src/auth_service/infrastructure/di/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ['create_container']
+
+from .container import create_container
--- src/auth_service/infrastructure/di/container.py
+++ src/auth_service/infrastructure/di/container.py
@@ -1,9 +1,15 @@
-from dishka import make_container
+from dishka import make_container, Container
 
 from .providers import InfrastructureProvider, PolicyProvider, UseCaseProvider
+from ..config import Config
 
-container = make_container(
-    InfrastructureProvider(),
-    PolicyProvider(),
-    UseCaseProvider(),
-)
+
+def create_container() -> Container:
+    config = Config.load()
+
+    return make_container(
+        InfrastructureProvider(),
+        PolicyProvider(),
+        UseCaseProvider(),
+        context={Config: config},
+    )
